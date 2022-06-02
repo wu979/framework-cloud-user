27ca0c62f4b79927b61d43e47a2cb1a190b5ba95
吴思伟 <18683789594@163.com>
2022-06-02 18:24:22 +0800

    first commit

---
+++ framework-cloud-user-api/src/test/java/com/framework/cloud/user/api/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framework.cloud.user.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
