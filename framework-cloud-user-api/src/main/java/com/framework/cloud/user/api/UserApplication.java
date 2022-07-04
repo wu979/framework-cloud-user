0c4365e748b5beea973a97d846c7ddf8e5e5a16a
吴思伟 <18683789594@163.com>
2022-07-04 17:50:46 +0800

    commit

--- framework-cloud-user-api/src/main/java/com/framework/cloud/user/api/UserApplication.java
+++ framework-cloud-user-api/src/main/java/com/framework/cloud/user/api/UserApplication.java
@@ -2,9 +2,11 @@ package com.framework.cloud.user.api;
 
 import com.framework.cloud.core.annotation.FrameworkApplication;
 import com.framework.cloud.feign.annotation.EnableFeignInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@Slf4j
 @EnableFeignInterceptor
 @EnableDiscoveryClient
 @FrameworkApplication
@@ -12,6 +14,7 @@ public class UserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+        log.info("User service started successfully");
     }
 
 }
