9553c7505d98ad41559be40170a377b6bd22b358
wusiwei <18683789594@163.com>
2022-12-27 15:51:37 +0800

    commit

--- framework-cloud-user-api/src/main/java/com/framework/cloud/user/api/application/pool/TaskPoolConfiguration.java
+++ framework-cloud-user-api/src/main/java/com/framework/cloud/user/api/application/pool/TaskPoolConfiguration.java
@@ -2,6 +2,7 @@ package com.framework.cloud.user.api.application.pool;
 
 import com.framework.cloud.executors.feature.ExecutorsFeature;
 import com.framework.cloud.user.common.constant.TaskPoolConstant;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.task.AsyncTaskExecutor;
@@ -13,11 +14,10 @@ import javax.annotation.Resource;
  *
  * @author wusiwei
  */
+@RequiredArgsConstructor
 public class TaskPoolConfiguration {
 
-    @Resource
-    @Qualifier("runs")
-    private ExecutorsFeature executorsFeature;
+    private final ExecutorsFeature executorsFeature;
 
     @Bean(TaskPoolConstant.COMMON)
     public AsyncTaskExecutor commonPool() {
