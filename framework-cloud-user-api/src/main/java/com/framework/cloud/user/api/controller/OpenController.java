040f0cc479e9aa5babcb3f30c15547ed6054a936
wusiwei <18683789594@163.com>
2023-01-10 16:25:46 +0800

    commit

--- framework-cloud-user-api/src/main/java/com/framework/cloud/user/api/controller/OpenController.java
+++ framework-cloud-user-api/src/main/java/com/framework/cloud/user/api/controller/OpenController.java
@@ -4,15 +4,15 @@ import com.framework.cloud.common.result.R;
 import com.framework.cloud.common.result.Result;
 import com.framework.cloud.common.utils.FastJsonUtil;
 import com.framework.cloud.event.publisher.ApplicationLocalPublisher;
+import com.framework.cloud.logging.annotation.Log;
 import com.framework.cloud.user.common.vo.PermissionRoleListVO;
+import com.framework.cloud.user.domain.entity.Role;
 import com.google.common.collect.Lists;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.event.TransactionalEventListener;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -35,12 +35,15 @@ public class OpenController {
         return R.success(Lists.newArrayList());
     }
 
+    @Log(note = "ceshi")
     @ApiOperation(value = "事件")
-    @GetMapping(value = "/event")
-    public Result<Boolean> event() {
+    @PostMapping(value = "/event")
+    public Result<Role> event(@RequestParam String tt) {
         TestEvent event = new TestEvent(this, 1);
         applicationLocalPublisher.publish(event);
-        return R.success(true);
+        Role role = new Role();
+        role.setName("111111111");
+        return R.success(role);
     }
 
 
